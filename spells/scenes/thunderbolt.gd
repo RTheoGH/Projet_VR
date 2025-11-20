2d02691e966060b769cfa5bfef08e934160722e5
VigKillian <viguier.killian05@gmail.com>
2025-11-20 19:55:32 +0100

    thunder spell on scene

--- spells/scenes/thunderbolt.gd
+++ spells/scenes/thunderbolt.gd
@@ -5,14 +5,17 @@ var explode_strength = 7.0;
 
 # Called when the node enters the scene tree for the first time.
 func _start_spell() -> void:
-	$GPUParticles3D.emitting = true
+	pass
 	
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	var explode_zone : Vector3 = $Area3D/CollisionShape3D2.global_position
-	var dist_sq: float = (body.global_position).distance_squared_to(explode_zone)
-	body.apply_central_impulse((body.global_position - explode_zone) * explode_strength)
-	HitManager.hit_body(body, explode_strength / dist_sq)
+	await get_tree().create_timer(13).timeout
+	$GPUParticles3D.emitting = true
+	if body is RigidBody3D and $GPUParticles3D.emitting :
+		var explode_zone : Vector3 = $Area3D/CollisionShape3D2.global_position
+		var dist_sq: float = (body.global_position).distance_squared_to(explode_zone)
+		body.apply_central_impulse((body.global_position - explode_zone) * explode_strength)
+		HitManager.hit_body(body, explode_strength / dist_sq)
 
 
 func _on_gpu_particles_3d_finished() -> void:
