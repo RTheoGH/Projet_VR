0dd249784931f90ed037cdbefe2c666ea990ea3f
Andrew Mansour <64646701+Sloth-King@users.noreply.github.com>
2025-11-21 11:12:32 +0100

    Fix interactable feather

--- src/scenes/draw_spell/feather_interactable.gd
+++ src/scenes/draw_spell/feather_interactable.gd
@@ -1,34 +1,43 @@
 extends XRToolsPickable
 
+var RAY_LENGTH = 1000
+
 var ink_decal = preload("res://src/scenes/draw_spell/ink_decal.tscn")
 var is_drawing = false
-var can_draw = false
 
-var max_decals = 50 # Technically can have more but it is counted with some types of lights so idk i'll limit it
+var max_decals = 200 # Technically can have more but it is counted with some types of lights so idk i'll limit it
 var active_decals : Array[Decal]
 var collision_body : Node3D
 
+var camera : Camera3D
+
+func _ready():
+	camera = get_viewport().get_camera_3d()
 
 func _physics_process(delta: float) -> void:
-	if is_drawing and can_draw :
+	if is_drawing:
 		draw_point(collision_body)
 
 func draw_point(body: Node3D):
+	var hit = cast_ray()
+	if hit:
 		if(active_decals.size() >= max_decals):
 			active_decals.pop_front().queue_free()
 			
 		var ink = ink_decal.instantiate()
-		ink.position = body.global_position
-		print("ink position : " , ink.position)
-		active_decals.append(ink)
-		add_child(ink)
+		ink.position = hit[0]
+		ink.rotation.x = atan2(hit[1].y , hit[1].z) + 90
+		ink.rotation.y = atan2(hit[1].x , hit[1].z)
+		ink.rotation.z = atan2(hit[1].x , hit[1].y)
 		
-func activate(_pressed: bool):
-	is_drawing = true
+		active_decals.append(ink)
+		get_parent().add_child(ink)
 	
+func activate(_pressed: bool):
+	is_drawing = _pressed
 
-func _on_tip_body_entered(body: Node3D) -> void:
-	#FIXME test actual conditions and see wagwan
-	if body != self:
-		can_draw = true
-		collision_body = body
+func cast_ray():
+	if $tip.get_collider() : 
+		return [$tip.get_collision_point(), $tip.get_collision_normal()]
+	return 
+	
