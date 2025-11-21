e258a6d0aaf135e2d71e6bd409ede035f57c6b69
Florentin Denis <florentin.denis07@gmail.com>
2025-11-21 09:57:30 +0100

    explosion et foudre

--- spells/scenes/fireball.gd
+++ spells/scenes/fireball.gd
@@ -4,6 +4,8 @@ var launch_strength: float = 10.0;
 var caster: Node3D
 var explode_strength = 5.0;
 
+var explosion = preload("res://spells/scenes/explosion.tscn")
+
 # Called when the node enters the scene tree for the first time.
 func _start_spell() -> void:
 	apply_central_impulse(to_global(Vector3.FORWARD * launch_strength))
@@ -14,7 +16,11 @@ func explode():
 			var dist_sq: float = (b.global_position).distance_squared_to(global_position)
 			b.apply_central_impulse((b.global_position - global_position) * explode_strength)
 			HitManager.hit_body(b, explode_strength / dist_sq)
+	var effect = explosion.instantiate()
+	effect.position = position
+	get_parent().add_child(effect)
 	queue_free()
 func _on_body_entered(body: Node) -> void:
 	explode()
 	print("boum")
+	
