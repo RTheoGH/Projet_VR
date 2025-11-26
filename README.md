e5edcbf20018d102c52be265c0e17feba73572ab
tom <tomben01@laposte.net>
2025-11-26 01:28:16 +0100

    qwak

---
+++ README.md
@@ -0,0 +1,6 @@
+# godot-duckdb
+
+This GDNative script aims to serve as a custom wrapper that makes DuckDB available in Godot 4.0+.
+It is heavily inspired by the [godot-sqlite](https://github.com/2shady4u/godot-sqlite) wrapper. **Alot** of the code base (even this README.md!) from the godot-sqlite repository has been used in this project.
+
+This is the orphaned 'assetlib'-branch that serves as a disconnected branch from which the Godot Asset Library downloads its files. 
--- src/globals/analytics.gd
+++ src/globals/analytics.gd
@@ -1,89 +1,99 @@
 extends Node
 
-
-
-var template_spell_data: Dictionary = {
-	"number_cast": 0,
-	"kills_by_spell": [] as Array[int],
-	
-}
-# should be const but then we can't use duplicate. Oh well...
-var template_session_data: Dictionary = {
-	"general": {
-		"meta": {
-			"player_age": 0,
-			"player_gender": "",
-			"player_proficiency_of_VR": 5, # [0, 10]
-			"player_proficiency_of_videogames": 5, # [0, 10]
-		}
-	},
-	"room1": {
-		"general": {
-			"completion_time": 0.0,
-			"time_before_first_spell": 0.0,
-			},
-		"spells": {
-			"fireball": template_spell_data.duplicate_deep(),
-			"antigrav": template_spell_data.duplicate_deep(),
-			"light":  template_spell_data.duplicate_deep(),
-			"lightning":  template_spell_data.duplicate_deep(),
-		}
-	},
-	"room2": {
-		# TODO etc
-	}
-}
-
-
 var save_directory: String = "./"
-var current_session_id: String
+var current_session_id: int
 var current_session_data: Dictionary
 
-func save_session() -> bool:
-	return false
-
-# call this. It saves
-func new_session(identifier: String) -> bool:
-	current_session_id = identifier
+var db : GDDuckDB
+func _ready():
+	var empty := not FileAccess.file_exists(save_directory + "db.parquet")
+	db = GDDuckDB.new()
+	db.set_path(save_directory + "db.parquet")
+	db.open_db()
+	db.connect()
 	
-	var could_save := save_session()
-	if not could_save:
-		return false
+	if empty:
+		db.query("
+		CREATE TABLE player (
+		    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+			age INT,
+			gender VARCHAR(32),
+			videogame_proficiency INT, 
+			vr_proficiency INT,
+		);
 		
-	current_session_data = template_session_data.duplicate_deep()
-	return true
+		CREATE TABLE system_balls (
+		    id INT NOT NULL,
+			positions REAL[3][],
+			completion_time REAL,
+			first_spell_time REAL,
+		);
+		")
+	db.get_query_result()
+	new_session()
+	set_value("player", "gender", "yolo")
 
-func set_value(path: String, value: Variant) -> void:
-	# access the dict as a path, for easier access
-	var path_elems: Array[String] = path.split("/", false)
-	var elem: String = path_elems.pop_back()
-	var current_dict_ref: Dictionary = current_session_data
-	for dict_name in path_elems:
-		if not current_dict_ref.has(dict_name):
-			push_error("ANALYTICS : Path not resolved : (", path, "). Please check the template in analytics.gd.")
-			return
-		current_dict_ref = current_dict_ref[dict_name]
-	
-	if not current_dict_ref.has(elem) or (typeof(elem) != typeof(current_dict_ref[elem])):
-			push_error("ANALYTICS : Wrong type at: (", path, ") : You tried to set this value : (", value, ") of type ", typeof(elem), "(expected ",  typeof(current_dict_ref[elem]), "). Please check the template in analytics.gd.")
-			return
-	current_dict_ref[elem] = value
+func show_error(text: String):
+	OS.alert(text, "Analytics error !!")
 
-func push_value(path: String, value: Variant) -> void:
-	# access the dict as a path, for easier access
-	var path_elems: Array[String] = path.split("/", false)
-	var elem: String = path_elems.pop_back()
-	var current_dict_ref: Dictionary = current_session_data
-	for dict_name in path_elems:
-		if not current_dict_ref.has(dict_name):
-			push_error("ANALYTICS : Path not resolved : (", path, "). Please check the template in analytics.gd.")
-			return
-		current_dict_ref = current_dict_ref[dict_name]
+func _exit_tree() -> void:
+	db.close_db()
+
+func get_current_id_from_db() -> int:
+	if db.query("SELECT MAX(id) FROM player AS id"):
+		if not db.get_query_result()[0]["max(id)"]:
+			return 0
+		
+		return db.get_query_result()[0]["max(id)"] + 1
+	else:
+		return -1
+
+func new_session() -> bool:
+	current_session_id = get_current_id_from_db() + 1
+	if current_session_id == -1:
+		show_error("Could not create a new analytics session!\nAnalytics will be deactivated for now")
+		return false
+	else:
+		db.query("INSERT INTO player DEFAULT VALUES ")
+		db.get_query_result()
+		db.query("INSERT INTO system_balls DEFAULT VALUES ")
+		db.get_query_result()
+		return true
+func set_value(table_name: String, attrib_name: String, value: Variant) -> void:
+	if not db.query("
+		UPDATE %s
+		SET %s = %s
+		WHERE id = %s
+	" % [table_name, attrib_name, str(value), str(current_session_id)]
+	):
+		show_error("Couldn't set %s into attribute %s of table %s" % [value, attrib_name, table_name])
+
+func push_value(table_name: String, attrib_name: String, value) -> void:
+	if value is Vector3:
+		value = "[%f, %f, %f]" % [value.x, value.y, value.z]
+	elif value is Vector2:
+		value = "[%f, %f]" % [value.x, value.y]
+	else:
+		value = str(value)
+	if db.query("
+		UPDATE %s
+		SET %s = list_append(
+			SELECT %s FROM %s WHERE id = %s,
+			%s
+		)
+		WHERE id = %s
+	" % [
+		table_name, attrib_name,
+		# internal select
+		attrib_name, table_name, str(current_session_id),
+		# end
+		value,
+		str(current_session_id)]
+	):
+		pass
+	else:
+		show_error("Couldn't push %s into attribute %s of table %s" % [value, attrib_name, table_name])
 	
-	if not current_dict_ref.has(elem) or (typeof(elem) != typeof(current_dict_ref[elem])):
-			push_error("ANALYTICS : Wrong type at: (", path, ") : You tried to push this value : (", value, ") of type ", typeof(elem), "(into array of type ",  typeof(current_dict_ref[elem]), "). Please check the template in analytics.gd.")
-			return
-	current_dict_ref[elem].push_back(value)
 
 func add_monitored(object: Object, var_name: String, targt_analytics_path: String):
 	pass
