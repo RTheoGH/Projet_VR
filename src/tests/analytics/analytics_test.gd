d84bfb8caf67d67bb4d532d6db6fb6b75fdb0492
tom <tomben01@laposte.net>
2026-01-07 22:03:12 +0100

    oui

--- src/tests/analytics/analytics_test.gd
+++ src/tests/analytics/analytics_test.gd
@@ -2,13 +2,12 @@ extends Node3D
 
 # fake player
 @onready var player := $CSGBox3D
-var player_name := "Jean-Jacques"
 
 func _ready():
 	player.global_position = Vector3(randf_range(-1, 1), randf_range(-1, 1), randf_range(-1, 1))
 	Analytics.set_value("player", "age", 10)
 	Analytics.set_value("player", "gender", "Ryan gosling")
-	Analytics.push_value("system_balls", "positions", player.global_position)
+	Analytics.add_monitored(player, "global_position", "system_balls", "positions")
 	print(Analytics.get_all_user_data())
 
 func _process(delta: float) -> void:
