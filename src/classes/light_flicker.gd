b0174696b1d39a12c752d21dd667bc529b5bf644
tom <tomben01@laposte.net>
2026-01-09 19:14:37 +0100

    lvel

---
+++ src/classes/light_flicker.gd
@@ -0,0 +1,12 @@
+@tool
+extends OmniLight3D
+
+@export var base_energy: float = 3.0
+@export_range(0.0, 1.0, 0.01) var flicker_strength: float = 1.0
+@export var flicker_speed: float = 1.0
+
+static var noise : FastNoiseLite = FastNoiseLite.new()
+
+func _process(delta:float):
+	var val: float = 0.1 * flicker_speed * ((Time.get_ticks_msec() % 10000) + global_position.length_squared())
+	light_energy = base_energy - (flicker_strength * base_energy * (noise.get_noise_1d(val) + 1.0) / 2.0)
