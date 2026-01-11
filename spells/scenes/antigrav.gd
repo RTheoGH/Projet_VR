eb13163f48e9ba0db6ae024a8266bac8d5dd2228
tom <tomben01@laposte.net>
2026-01-11 14:49:53 +0100

    antigrav fix

--- spells/scenes/antigrav.gd
+++ spells/scenes/antigrav.gd
@@ -24,6 +24,12 @@ func expand():
 		Vector3(1.0, 1.0, 1.0),
 		1.0
 	).set_trans(Tween.TRANS_CUBIC)
+	tween.parallel().tween_property(
+		$MeshInstance3D,
+		"scale",
+		Vector3(1.0, 1.0, 1.0) * 6.0,
+		1.0
+	).set_trans(Tween.TRANS_CUBIC)
 	await tween.finished
 	
 	await get_tree().create_timer(5.0).timeout
@@ -40,11 +46,19 @@ func expand():
 		Vector3(0.0, 0.0, 0.0),
 		1.0
 	).set_trans(Tween.TRANS_CUBIC)
+	
+	tween.parallel().tween_property(
+		$MeshInstance3D,
+		"scale",
+		Vector3(0.0, 0.0, 0.0),
+		1.0
+	).set_trans(Tween.TRANS_CUBIC)
 	await tween.finished
 	queue_free()
 func _on_body_entered(_body: Node) -> void:
 	freeze = true
 	$Area3D/CollisionShape3D2.disabled = false
+	$CollisionShape3D.disabled = true
 	$expand.play()
 	expand()
 
